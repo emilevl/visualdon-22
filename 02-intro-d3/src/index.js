56731796d43288a87b1f25516d9a8c646dc5362a
Emile <emile.vianneyliaud@gmail.com>
2022-05-31 15:14:23 +0200

    trying the api

--- 02-intro-d3/src/index.js
+++ 02-intro-d3/src/index.js
@@ -4,135 +4,271 @@ import * as d3 from 'd3';
 
 const body = d3.select("body");
 
-body.append("svg")
-    .attr("width", 500)
-    .attr("height", 500)
-    .attr("class", "svg-circles1")
-    .append("circle")
-    .attr("class", "circle1")
-    .attr("cx", "50")
-    .attr("cy", "50")
-    .attr("r", "40");
 
-const svgCircles1 = d3.select(".svg-circles1");
+const width = window.innerWidth,
+height = window.innerHeight;
 
-svgCircles1.append("circle")
-    .attr("class", "circle2")
-    .attr("cx", "150")
-    .attr("cy", "150")
-    .attr("r", "40");
-
-svgCircles1.append("circle")
-    .attr("class", "circle3")
-    .attr("cx", "250")
-    .attr("cy", "250")
-    .attr("r", "40");
-
-const circle1 = d3.select(".circle1");
-const circle2 = d3.select(".circle2");
-const circle3 = d3.select(".circle3");
-
-//Changez la couleur du deuxième cercle
-//Déplacez de 50px vers la droite le premier et le deuxième cercle
-circle2.attr("fill", "red").attr("transform", "translate(50, 0)");
-circle1.attr("transform", "translate(50, 0)");
-
-//Rajoutez du texte en dessous de chaque cercle2
-svgCircles1.append("text").attr("x", circle1.attr("cx")).attr("y", (parseInt(circle1.attr("cy")) + parseInt(circle1.attr("r")) + 20)).text("Hello");
-svgCircles1.append("text").attr("x", circle2.attr("cx")).attr("y", (parseInt(circle2.attr("cy")) + 100)).text("Hello");
-svgCircles1.append("text").attr("x", circle3.attr("cx")).attr("y", (parseInt(circle3.attr("cy")) + 100)).text("Hello");
+const h = width/10
+const w = height/10;
 
 
-//Alignez verticalement les cercles en cliquant sur le dernier cercle
-circle3.on("click", () => {
-    svgCircles1.selectAll("circle").attr("cx", 200).attr("transform", "translate(0,0)");
-    } )
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-//Vous avez à disposition les données suivantes: [20, 5, 25, 8, 15]
-//Ces données représentent la hauteur des rectangles que vous allez dessiner avec la méthode data(data).enter() 
-// que nous avons vue en cours. Les rectangles auront une largeur fixe de 20px et doivent être alignés en bas 
-// l'un à côté de l'autre (comme un graphique en batons ! 📊 )
-const data = [20, 5, 25, 8, 15];
+    // let svgSieges = d3.select(".sieges").width(width).height(height);
 
-body.append("div").append("svg")
-  .attr("width", 500)
-  .attr("height", 500)
-  .attr("class", "svg-rect1");
+// append img comfortable-chair-svgrepo-com.svg as an svg to the body
+// var width = 800,
+//     height = 800;
+// let img = await(d3.xml('/comfortable-chair-svgrepo-com.svg'));
 
-  const svgRect = d3.select(".svg-rect1");
-//   <rect x="40" y="40" width="60" height="30"></rect>
-console.log(data.length)
-svgRect.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 30)
-    .attr("y", 40)
-    .attr("width", 20)
-    .attr("height", d => d*10)
-    .attr("transform", "rotate(180), translate(-300, -500)")
+// d3.xml('myIllustration.svg')
+//     .then(data => {
+//         d3.select('body').node().append(data.documentElement)
+//     })
+let siege = d3.select('.siege')
+
+// g.append('svg')
+//   .attr('width', 30)
+//   .attr('height', 30)
+//   .append(() => img.documentElement);
 
 
+// var img = g.append("svg:image")
+//     .attr("xlink:href", "/comfortable-chair-svgrepo-com.svg")
+//     .attr("width", 30)
+//     .attr("height", 30)
+//     .attr("x", 0)
+//     .attr("y",0);
+// let newSiege = siege.clone(true)
+// .attr('x', 62)
+// .attr('y', 50)
+// .attr('width', 30)
+let iX = 0;
+let iY = 0;
+for(let i = 1; i < 500; i++){
+    let eachImg = siege.clone(true)
+      .attr("transform", "translate(" + translateX(i) + ", " + translateY(i) + ")")
+      .attr('class', () => {
+        if (disney){
+          return 'disney';
+        }else if (pixar) {
+          return 'pixar'
+        }else {
+          return 'empty'
+        }
+      })
+      // .fill('fill', () => {
+      //   return 'blue'
+      // })
+}
+
+function translateX(i) {
+  let positionX = 0;
+  if (i%10 == 0) {
+    iX = 0;
+  } else {
+    iX ++;
+  }
+  positionX = iX * 50;
+
+  return positionX
+}
+
+function translateY(i) {
+  let positionY = 0;
+  if (i%10 == 0) {
+    iY ++;
+  }
+  positionY = iY * 100;
+
+  return positionY
+}
 
 
-    /********************************************** */
+// const testsvg = () =>{
+//     const svg = d3.create('svg').attr("width", w).attr("height", h)
+//     const main = svg.append("g").attr('id','test')
+   
+//     main.append('circle')
+//       .attr('fill','red')
+//       .attr("cx", w/2)
+//       .attr("cy", h/2)
+//       .attr("r", w/2);
+  
+//     main.append('line')
+//       .attr('id','teLine')
+//       .attr('x1',w/2)
+//       .attr('y1',0)
+//       .attr('x2',w/2)
+//       .attr('y2',w)
+//       .attr('stroke','black')
+//       .attr('transform',`rotate(45,${w/2},${h/2})`)
+  
+//     return svg.node()
+//   }
 
-// Select body and add svg to it
-const newBody = d3.select("body").append("svg").attr("width", 600).attr("height", 600);
+// const svg2 = () => {
+//     let i = 0
+//     const svg = d3.create('svg').attr("width", width).attr("height", width)
+//     const main2 = svg.append("g").attr('id','main2')
 
-// Append first circle
-newBody.append('svg').append('circle')
-  .attr('cx', 50)
-  .attr('cy', 50)
-  .attr('r', 40)
-  .attr('fill', '#6cba61');
-// Append second circle
-newBody.append('svg').append('circle')
-.attr('cx', 150)
-.attr('cy', 150)
-.attr('r', 40)
-.attr('fill', '#7f05aa')
-.attr("transform","translate(50,0)");
-// Append third circle
-newBody.append('svg').append('circle')
-  .attr('cx', 250)
-  .attr('cy', 250)
-  .attr('r', 40)
-  .attr('fill', '#d6563a')
-  .attr("transform","translate(50,0)")
-  .attr("id","lastCircle");
+//     let other = d3.select(testsvg)
+//     let group = other.selectAll('#test').clone(true);
 
-d3.selectAll('circle')
-    .each(function(){
-    d3.select(this.parentNode).append("text")
-    .text('Circle')
-    .attr("x",d3.select(this.parentNode).node().getBBox().width-60)
-    .attr("y",d3.select(this.parentNode).node().getBBox().height)
-})
+//     let c = main2.selectAll('g')
+//         .data(data).join('g')
+//         .attr("transform", ([i, j]) => `translate(${i},${j})`)
+//         .attr('id', (d,i) => `C2_${i}`)
+//         .append(() => group.clone(true).node())
+        
+//     return svg.node()
+// }
+// body.append(testsvg)
 
-d3.select("#lastCircle").on("click", function(){
-    d3.selectAll('circle')
-    .each(function(){
-        d3.select(this)
-        .transition()
-        .attr("cx", d3.select(this.parentNode.parentNode).node().getBBox().width/2)
-        .attr("transform","none");
-        d3.selectAll('text')
-        .transition()
-        .attr("x",d3.select(this.parentNode.parentNode).node().getBBox().width/2-20);
-    })
-});
+// body.append(svg2)
+// const data = d3.cross(d3.range(0,width,w), d3.range(0,width,h))
 
-var body2 = d3.select("body");
+// body.append("svg")
+//     .attr("width", 500)
+//     .attr("height", 500)
+//     .attr("class", "svg-test")
+//     .append("circle")
+//     .attr("class", "circle1")
+//     .attr("cx", "50")
+//     .attr("cy", "50")
+//     .attr("r", "40");
 
-var dataRect = [20, 5, 25, 8, 15];
+// body.append("svg")
+//     .attr("width", 500)
+//     .attr("height", 500)
+//     .attr("class", "svg-circles1")
+//     .append("circle")
+//     .attr("class", "circle1")
+//     .attr("cx", "50")
+//     .attr("cy", "50")
+//     .attr("r", "40");
 
-body2.data(dataRect)
-.enter()
-.append('svg')
-.attr("height", function(h) { return h })
-.attr("width", 30)
-.append("rect")
-.attr("width", 20)
-.attr("height", function(h) { return h })
-.attr("fill", "#69b3a2");
+// const svgCircles1 = d3.select(".svg-circles1");
+
+// svgCircles1.append("circle")
+//     .attr("class", "circle2")
+//     .attr("cx", "150")
+//     .attr("cy", "150")
+//     .attr("r", "40");
+
+// svgCircles1.append("circle")
+//     .attr("class", "circle3")
+//     .attr("cx", "250")
+//     .attr("cy", "250")
+//     .attr("r", "40");
+
+// const circle1 = d3.select(".circle1");
+// const circle2 = d3.select(".circle2");
+// const circle3 = d3.select(".circle3");
+
+// //Changez la couleur du deuxième cercle
+// //Déplacez de 50px vers la droite le premier et le deuxième cercle
+// circle2.attr("fill", "red").attr("transform", "translate(50, 0)");
+// circle1.attr("transform", "translate(50, 0)");
+
+// //Rajoutez du texte en dessous de chaque cercle2
+// svgCircles1.append("text").attr("x", circle1.attr("cx")).attr("y", (parseInt(circle1.attr("cy")) + parseInt(circle1.attr("r")) + 20)).text("Hello");
+// svgCircles1.append("text").attr("x", circle2.attr("cx")).attr("y", (parseInt(circle2.attr("cy")) + 100)).text("Hello");
+// svgCircles1.append("text").attr("x", circle3.attr("cx")).attr("y", (parseInt(circle3.attr("cy")) + 100)).text("Hello");
+
+
+// //Alignez verticalement les cercles en cliquant sur le dernier cercle
+// circle3.on("click", () => {
+//     svgCircles1.selectAll("circle").attr("cx", 200).attr("transform", "translate(0,0)");
+//     } )
+
+// //Vous avez à disposition les données suivantes: [20, 5, 25, 8, 15]
+// //Ces données représentent la hauteur des rectangles que vous allez dessiner avec la méthode data(data).enter() 
+// // que nous avons vue en cours. Les rectangles auront une largeur fixe de 20px et doivent être alignés en bas 
+// // l'un à côté de l'autre (comme un graphique en batons ! 📊 )
+// const data = [20, 5, 25, 8, 15];
+
+// body.append("div").append("svg")
+//   .attr("width", 500)
+//   .attr("height", 500)
+//   .attr("class", "svg-rect1");
+
+//   const svgRect = d3.select(".svg-rect1");
+// //   <rect x="40" y="40" width="60" height="30"></rect>
+// console.log(data.length)
+// svgRect.selectAll("rect")
+//     .data(data)
+//     .enter()
+//     .append("rect")
+//     .attr("x", (d, i) => i * 30)
+//     .attr("y", 40)
+//     .attr("width", 20)
+//     .attr("height", d => d*10)
+//     .attr("transform", "rotate(180), translate(-300, -500)")
+
+
+
+
+//     /********************************************** */
+
+// // Select body and add svg to it
+// const newBody = d3.select("body").append("svg").attr("width", 600).attr("height", 600);
+
+// // Append first circle
+// newBody.append('svg').append('circle')
+//   .attr('cx', 50)
+//   .attr('cy', 50)
+//   .attr('r', 40)
+//   .attr('fill', '#6cba61');
+// // Append second circle
+// newBody.append('svg').append('circle')
+// .attr('cx', 150)
+// .attr('cy', 150)
+// .attr('r', 40)
+// .attr('fill', '#7f05aa')
+// .attr("transform","translate(50,0)");
+// // Append third circle
+// newBody.append('svg').append('circle')
+//   .attr('cx', 250)
+//   .attr('cy', 250)
+//   .attr('r', 40)
+//   .attr('fill', '#d6563a')
+//   .attr("transform","translate(50,0)")
+//   .attr("id","lastCircle");
+
+// d3.selectAll('circle')
+//     .each(function(){
+//     d3.select(this.parentNode).append("text")
+//     .text('Circle')
+//     .attr("x",d3.select(this.parentNode).node().getBBox().width-60)
+//     .attr("y",d3.select(this.parentNode).node().getBBox().height)
+// })
+
+// d3.select("#lastCircle").on("click", function(){
+//     d3.selectAll('circle')
+//     .each(function(){
+//         d3.select(this)
+//         .transition()
+//         .attr("cx", d3.select(this.parentNode.parentNode).node().getBBox().width/2)
+//         .attr("transform","none");
+//         d3.selectAll('text')
+//         .transition()
+//         .attr("x",d3.select(this.parentNode.parentNode).node().getBBox().width/2-20);
+//     })
+// });
+
+// var body2 = d3.select("body");
+
+// var dataRect = [20, 5, 25, 8, 15];
+
+// body2.data(dataRect)
+// .enter()
+// .append('svg')
+// .attr("height", function(h) { return h })
+// .attr("width", 30)
+// .append("rect")
+// .attr("width", 20)
+// .attr("height", function(h) { return h })
+// .attr("fill", "#69b3a2");
--- 03-d3-data/src/index.js
+++ 03-d3-data/src/index.js
@@ -2,7 +2,9 @@ import * as d3 from 'd3';
 import { csv, json } from 'd3-fetch'
 
 const body = d3.select("body");
-const arrayUserPost = []
+const arrayUserPost = [];
+let svg, x, y, height, 
+i = 0;
 
 console.log("Test");
 Promise.all([
@@ -91,25 +93,26 @@ d3.select("body")
 // Graphique bâton
 
 var margin = {top: 20, right: 10, bottom: 60, left: 60};
-var width = 1500 - margin.left - margin.right,
+var width = 1500 - margin.left - margin.right
+
 height = 500 - margin.top - margin.bottom;
 
 d3.select("body")
     .append("div")
     .attr('id', 'graph')
-let svg = d3.select("#graph")
+svg = d3.select("#graph")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-let x = d3.scaleBand()
+x = d3.scaleBand()
     .domain(arrayUserPost.map(function(d) { return d["nom_utilisateur"]; }))
     .range([ 1000, 0]);
     
 
-let y = d3.scaleLinear()
+y = d3.scaleLinear()
     .domain(arrayUserPost.map(function(d) { return d["posts"].length; }))
     .range([ height, 0]);
 
@@ -124,14 +127,135 @@ svg.append("g")
     .selectAll("text")
 .attr("transform", "translate(-2,10)")
 
-svg.selectAll("bars")
+
+
+draw();
+
+});
+
+function draw() {
+    
+    x = d3.scaleBand()
+    .domain(arrayUserPost.map(function(d) { return d["nom_utilisateur"]; }))
+    .range([ 1000, 0]);
+
+
+    y = d3.scaleLinear()
+    .domain(arrayUserPost.map(function(d) { return d["posts"].length; }))
+    .range([ height, 0]);
+
+    i++;
+    svg.selectAll("rect")
     .data(arrayUserPost)
-    .enter()
-    .append("rect")
+    .join(enter => enter
+        .append("rect")
         .attr("x", function(d) { return x(d["nom_utilisateur"])+40; })
         .attr("y", function(d) { return y(d["posts"].length); })
         .attr("width", "20px")
         .attr("height", function(d) { return height - y(d["posts"].length); })
-        .attr("fill", "#69b3a2")
+        .attr("fill", "#69b3a2"),
+    update => update
+        .attr("x", function(d) { return x(d["nom_utilisateur"]) + 40; })
+        .attr("y", function(d) { return y(d["posts"].length); })
+        .attr("width", "20px")
+        .attr("height", function(d) { return height - i  - y(d["posts"].length); })
+        .attr("fill", "#69b3a2"),
+    exit => exit
+        .remove());
+}
+
+document.getElementById("change").addEventListener("click", function() {
+    // console.log(arrayUserPost)
+    
+    arrayUserPost.forEach((user, idx) => {
+        user["nom_utilisateur"] = 'hello' + idx;
+    })
+    draw();
 });
 
+////////////////////////////////////////////
+
+filmsAComparer[0] = Disney.find(element => element.title == selectDisney.options[selectDisney.selectedIndex].value)
+filmsAComparer[1] = Pixar.find(element => element.title == selectPixar.options[selectPixar.selectedIndex].value)
+console.log(Disney.find(element => element.title == selectDisney.options[selectDisney.selectedIndex].value));
+console.log(filmsAComparer);
+
+// svg
+d3.select("body")
+    .append("div")
+    .attr('id', 'barres')
+let barres = d3.select("#barres")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+// //Axe X
+// let x = d3.scaleBand()
+//     .domain(filmsAComparer.map(function (d) {
+//         return d.title;
+//     }))
+//     .range([0, 500]);
+
+// //Axe y
+// let y = d3.scaleLinear()
+//     .domain([0, 10])
+//     .range([height, 0]);
+
+
+// barres.append("g")
+//     .call(d3.axisLeft(y))
+//     .attr("transform", "translate(" + 250 + ", 0 )");
+
+// barres.append("g")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x))
+//     .selectAll("text")
+//     .attr("transform", "translate(-2,10)")
+
+
+filmsAComparer[0] = Disney[0]
+filmsAComparer[1] = Pixar[0]
+dessinerBarres(filmsAComparer, barres, height);
+
+//données
+function dessinerBarres(filmsAComparer, barres) {
+    //Axe X
+    let x = d3.scaleBand()
+        .domain(filmsAComparer.map(function (d) {
+            return d.title;
+        }))
+        .range([0, 500]);
+
+    //Axe y
+    let y = d3.scaleLinear()
+        .domain([0, 10])
+        .range([height, 0]);
+
+
+    barres.append("g")
+        .call(d3.axisLeft(y))
+        .attr("transform", "translate(" + 250 + ", 0 )");
+
+    barres.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-2,10)")
+    barres.selectAll("bars")
+        .data(filmsAComparer)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) {
+            return x(d.title) + 115;
+        })
+        .attr("y", function (d) {
+            return y(d.imdb);
+        })
+        .attr("width", "20px")
+        .attr("height", function (d) {
+            return height - y(d.imdb);
+        })
+        .attr("fill", "#000000")
+}
